da072886fcac706b87264c8b195598f5efc40029
rcampbell30 <camperbell19@gmail.com>
2026-09-06 06:43:11 +0100

    Load data page styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App'
 import './styles.css'
 import './evidence.css'
 import './responsive.css'
+import './data-pages.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
