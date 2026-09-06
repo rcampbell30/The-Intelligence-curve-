bb0e0aa74f40b3347f20265972a06705b99a1b61
rcampbell30 <camperbell19@gmail.com>
2026-09-06 06:47:48 +0100

    Load interactive dashboard styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './styles.css'
 import './evidence.css'
 import './responsive.css'
 import './data-pages.css'
+import './interactive.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
